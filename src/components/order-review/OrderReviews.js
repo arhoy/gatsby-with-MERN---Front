04a7f1401bfc653a604216dffca3c97e49fc7f7c
arhoy <arhoy@ualberta.ca>
2019-12-07 18:05:03 -0700

    review toggle added

--- src/components/order-review/OrderReviews.js
+++ src/components/order-review/OrderReviews.js
@@ -1,8 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import { connect } from 'react-redux';
 import { getReviewForSlug } from '../../actions/review';
 import OrderReview from './OrderReview';
+
+const Div = styled.div`
+  & select {
+    padding: 1rem;
+    border-radius: 0.5rem;
+    margin: 1rem 0;
+  }
+`;
+
+const SelectContainer = styled.div``;
 
 const ReviewsContainer = styled.div`
   display: flex;
@@ -17,17 +27,48 @@ const OrderReviews = ({
   slug,
 }) => {
   useEffect(() => {
-    getReviewForSlug(slug);
+    getReviewForSlug(slug, '-createdAt');
   }, [getReviewForSlug]);
+
+  // select dropdown options
+  const sortOptions = [
+    { label: 'Most Recent', value: '-createdAt' },
+    { label: 'Rating (high to low)', value: '-rating' },
+    { label: 'Rating (low to high)', value: 'rating' },
+  ];
+
+  const [sort, setSort] = useState(sortOptions[0].value);
+
+  const selectSorHandler = e => {
+    setSort(e.target.value);
+
+    getReviewForSlug(slug, e.target.value);
+  };
+
+  // rendering
 
   if (loading) return <div> I am loading! </div>;
 
   return (
-    <ReviewsContainer>
-      {reviews &&
-        reviews.length > 0 &&
-        reviews.map(review => <OrderReview key={review._id} review={review} />)}
-    </ReviewsContainer>
+    <Div>
+      <div> sorting by {sort} </div>
+      <SelectContainer>
+        <select value={sort} onChange={e => selectSorHandler(e)}>
+          {sortOptions.map(o => (
+            <option key={o.label} value={o.value}>
+              {o.label}
+            </option>
+          ))}
+        </select>
+      </SelectContainer>
+      <ReviewsContainer>
+        {reviews &&
+          reviews.length > 0 &&
+          reviews.map(review => (
+            <OrderReview key={review._id} review={review} />
+          ))}
+      </ReviewsContainer>
+    </Div>
   );
 };
 
